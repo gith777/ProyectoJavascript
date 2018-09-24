c6a5ccf1e0fb819c06801b83625d11c6314696da
Manuel Venegas <manuelalexander08@gmail.com>
2018-09-24 16:12:55 -0500

    Pruebas con variables de estructuras Basicas

--- script/app.js
+++ script/app.js
@@ -1,1 +1,47 @@
-console.log("Hola Mundo");
+/*
+var numero1=10.5;
+var numero2=5;
+var resultado= numero1*numero2;
+
+console.log(resultado);
+
+var nombre ='Yefersson ';
+var profesion ="Desarrollador Full Stack";
+
+console.log(nombre + profesion);
+
+var tiene_mascota=true;
+
+console.log(tiene_mascota);
+
+var datos=["Juan",26,false,25,78,"Desarrollador"];
+
+console.log(datos[2]);
+
+
+
+var edad = 41;
+
+if(edad>=18 && edad<=40){
+	console.log("Eres un adulto JOVEN!");
+}
+else{
+	console.log("No! eres un adulto JOVEN...");
+}
+
+var dias=["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+
+for(var cont=0; cont<7; cont++){
+
+	console.log(dias[cont]);
+}
+
+*/
+
+function suma(numero1, numero2){
+	var resultado= numero1 + numero2;
+	return resultado;
+}
+
+var res=suma(88, 55);
+console.log(res);
