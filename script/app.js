6884d371b6e3e70d85bbb1c263fdcd0c99e6a1a8
Manuel Venegas <manuelalexander08@gmail.com>
2018-09-25 15:50:13 -0500

    Aplicacion con JS -Calcular IVA

--- script/app.js
+++ script/app.js
@@ -45,22 +45,45 @@ function suma(numero1, numero2){
 
 var res=suma(88, 55);
 console.log(res);
-*/
 
-<<<<<<< HEAD
-function ivaproducto(valor_producto){
-=======
-function ivaProducto(valor_Producto){
->>>>>>> 9ab1a8cc658387b350c9259cffeb2f02a2aa37a1
+
+
+function ivaproducto(valor_Producto){
 	var iva = valor_Producto*0.19;
 	return iva;
 }
 
-<<<<<<< HEAD
-
 var resul=ivaproducto(20000);
-=======
-var resul=ivaProducto(20000);
->>>>>>> 9ab1a8cc658387b350c9259cffeb2f02a2aa37a1
+
 console.log(resul);
+
+
+var titulos = document.getElementsByTagName('h1');
+
+console.log(titulos[0]);
+
+var clasetexto= document.getElementsByClassName('texto');
+
+console.log(clasetexto);
+
+var sede= document.getElementById("sede");
+
+console.log(sede);
+*/
+
+function iva(precio_Producto, porcentaje_iva){
+	var iva_producto = precio_Producto* (porcentaje_iva/100);
+	return iva_producto;
+}
+
+function calcular_iva(){
+	var precio= parseInt(document.getElementById('precio').value);
+	var porcentaje_iva= parseInt(document.getElementById('iva').value);
+	var resultado= iva(precio, porcentaje_iva);
+    var salida= document.getElementById('resultado');
+    var texto=document.createTextNode(resultado);
+
+    salida.value=resultado;	
+}
+
 
